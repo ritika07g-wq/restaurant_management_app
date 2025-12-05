50ada3df8426eebf43b4139c88d4ed1572c6980c
ritika07g-wq <ritika0725g@gmail.com>
2025-12-06 00:20:09 +0530

    Updated project files and CI/CD setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/presentation/splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
+// Import your screens
+import 'presentation/splash/splash_screen.dart';
+import 'presentation/login/screenlogin.dart';
+import 'presentation/manager/manager_page.dart';
+import 'presentation/chef/chef_page.dart';
+import 'presentation/chef/menu_page.dart';
+import 'presentation/staff/staff_page.dart';
+import 'presentation/settings/settingspage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Restaurant Management App',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home:SplashScreen(),
+
+      // 👇 Your first screen
+      home: const SplashScreen(),
+
+      // 👇 Named routes (optional but useful)
+      routes: {
+        '/login': (context) => const Screenlogin(),
+        '/manager': (context) => ManagerPage(managerName: 'Default Manager'),
+        '/chef': (context) => const ChefPage(),
+        '/menu': (context) => const MenuPage(),
+        '/staff': (context) => StaffPage(staffName: 'Staff', staffRole: 'Chef'),
+        '/settings': (context) => SettingsPage(role: 'Admin'),
+      },
     );
   }
 }
-
